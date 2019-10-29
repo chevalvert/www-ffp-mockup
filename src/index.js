d3619dc74cc22e8177038aba814f24ac65bfaf30
RNO <arnaud.juracek@gmail.com>
2019-10-29 15:17:52 +0100

    Module fade on scroll

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-/* global IntersectionObserver */
-import 'nodelist-foreach'
 import barba from 'controllers/barba'
 import lazyload from 'controllers/lazyload'
+import fadeOnScroll from 'controllers/fade-on-scroll'
 import FFPLandscape from 'controllers/ffp-landscape'
-
 import menu from 'components/menu'
 
 import 'views/article'
@@ -26,23 +24,8 @@ barba({
   newPageReady: () => {
     menu()
     lazyload()
+    fadeOnScroll()
     FFPLandscape.build()
-
-    // TODO: module
-    // TODO: check for intersection observer
-    const observer = new IntersectionObserver(entries => {
-      entries.forEach(entry => {
-        if (entry.target && entry.intersectionRatio > 0) {
-          entry.target.classList.add('is-visible')
-        }
-      })
-    }, { rootMargin: '20px 0px 70px 0px' })
-    const fadables = document.querySelectorAll('.barba-container > *')
-    fadables.forEach(el => {
-      el.setAttribute('js-wait-for-intersection', true)
-      observer.observe(el)
-      el.addEventListener('mouseenter', () => el.classList.add('is-visible'))
-    })
   },
 
   transitionCompleted: () => {
