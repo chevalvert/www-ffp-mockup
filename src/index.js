c7809504d3b4ea6bf74d287d4578428fcfe8c88d
RNO <arnaud.juracek@gmail.com>
2020-02-18 11:49:47 +0100

    Remove barba.js logic

--- src/index.js
+++ src/index.js
@@ -1,38 +1,44 @@
-import barba from 'controllers/barba'
-import lazyload from 'controllers/lazyload'
+import 'nodelist-foreach'
+
 import fadeOnScroll from 'controllers/fade-on-scroll'
 import FFPLandscape from 'controllers/ffp-landscape'
+import lazyload from 'controllers/lazyload'
 import menu from 'components/menu'
+import photoswipe from 'controllers/photoswipe'
+import sortTable from 'controllers/table-sort'
 
-import 'views/article'
-import 'views/table'
-import 'views/form'
+// Global templates
+menu()
+lazyload()
+fadeOnScroll()
+FFPLandscape.build()
 
-import defaultTransition from 'views/transitions/default'
+// Article templates
+photoswipe({ gallerySelector: '.article__gallery' })
 
-barba({
-  wrapperId: 'main',
-  containerClass: 'barba-container',
-  updateOutsideWrapper: ['.menu', '.menu--mobile'],
+// Form templates
+{
+  const inputFiles = document.querySelectorAll('.better-input-file')
+  inputFiles.forEach(label => {
+    const id = label.getAttribute('for')
+    const input = document.getElementById(id)
+    input.addEventListener('change', e => {
+      const filenames = Array.from(input.files).map(f => f.name)
+      label.setAttribute('data-files', filenames.join('\r\n'))
+    })
+  })
+}
 
-  linkClicked: () => {
-    document.body.classList.add('is-loading')
-    document.body.removeAttribute('no-scroll')
-    FFPLandscape.destroy()
-  },
+// Table templates
+{
+  sortTable()
 
-  newPageReady: () => {
-    menu()
-    lazyload()
-    fadeOnScroll()
-    FFPLandscape.build()
-  },
-
-  transitionCompleted: () => {
-    document.body.classList.remove('is-loading')
-  },
-
-  transitionsMap: {
-    default: defaultTransition
-  }
-})
+  const form = document.querySelector('form')
+  const resetButton = form && form.querySelector('button[type="reset"]')
+  resetButton && resetButton.addEventListener('click', e => {
+    e.preventDefault()
+    const fields = form.querySelectorAll('input, textarea, select, checkbox, radio')
+    fields.forEach(field => { field.value = '' })
+    form.submit()
+  })
+}
--- src/views/transitions/default.js
+++
@@ -1,4 +0,0 @@
-export default (oldContainer, newContainer, lastClickEl, done) => {
-  if (lastClickEl && !lastClickEl.hasAttribute('data-lang-switcher')) window.scrollTo(0, 0)
-  return done()
-}
