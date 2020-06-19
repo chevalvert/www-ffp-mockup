487667da45fe4180576de4f0f7fbfc3a8090690f
RNO <arnaud.juracek@gmail.com>
2020-06-19 11:37:43 +0200

    Expose in the lazyload method in the window object
    This will be useful in WP to re-trigger lazyload outside the page
    lifecycle

--- src/controllers/lazyload.js
+++ src/controllers/lazyload.js
@@ -2,13 +2,14 @@ import 'intersection-observer'
 import 'nodelist-foreach'
 import lozad from 'lozad'
 
-export default ({
-  selector = '[data-lazyload]'
-} = {}) => {
+window.lazyload = function ({ selector = '[data-lazyload]' } = {}) {
   let observer = lozad(selector, {
     rootMargin: '512px 0px',
     threshold: 0.1
   })
-
   observer.observe()
+
+  return observer
 }
+
+export default window.lazyload
